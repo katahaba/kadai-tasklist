e629e89fe329bacad68a45a7c39252f20b46e2d7
necocat <noctiluca1234@gmail.com>
2018-07-01 08:56:46 +0000

    table formed

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,12 +1,24 @@
 @extends('layouts.app')
 @section('content')
     <h1>タスク一覧</h1>
+    <p>(編集・削除はID番号をクリック)</p>
     
     @if (count($tasks)>0)
         <ul>
-            @foreach ($tasks as $task) 
-               <li>{!! link_to_route('tasks.show',$task->id,['id'=>$task->id]) !!}: ステータス: {{ $task->status }} , タスク名: {{ $task->content }}</li>
-            @endforeach
+            <table class="table table-striped table-bordered table-condensed">
+                <tr>
+                    <th class="text-center">ID</th>
+                    <th class="text-center">Status</th>
+                    <th class="text-center">Task_Name</th>
+                </tr>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td class="text-center">{!! link_to_route('tasks.show',$task->id,['id'=>$task->id]) !!}</td>
+                        <td class="text-center">{{ $task->status }}</td>
+                        <td class="text-center">{{ $task->content }}</td>
+                    </tr>
+                @endforeach
+            </table>
         </ul>
     @endif
     
