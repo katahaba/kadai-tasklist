3132a4889ef930ca2167565fb16fa5c82a25a040
necocat <noctiluca1234@gmail.com>
2018-07-03 13:43:29 +0000

    user pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     @if (Auth::check())
-        <?php $user= Auth::user(); ?>
+        <?php $user = Auth::user(); ?>
         {{ $user->name }}
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Tasklist</h1>
-                {!! lonk_to_route('signup.get', 'Sign up now!', null, ['class', => 'btn btn-lg btn-primary']) !!}
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
     @endif
