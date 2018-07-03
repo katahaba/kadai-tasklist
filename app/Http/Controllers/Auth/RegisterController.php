89f7697ab56e16fab5a37c0a708010951ee0b776
necocat <noctiluca1234@gmail.com>
2018-07-03 13:41:37 +0000

    user pages

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -20,14 +20,14 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+    use RegistersUsers;//トレイト　いろんなアクションがまとめて入ってる
 
     /**
      * Where to redirect users after registration.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,17 @@
 */
 Route::get('/','TasksController@index');
 Route::resource('tasks','TasksController');
+
+//ユーザー登録
+Route::get('signup', "Auth\RegisterController@showRegistrationForm")->name('signup.get');
+Route::post('signup', "Auth\RegisterController@register")->name('signup.post');
+
+//ログイン認証
+Route::get('login', "Auth\LoginController@showLoginForm")->name('login');
+Route::post('login', "Auth\LoginController@login")->name('login.post');
+Route::get('logout', "Auth\LoginController@logout")->name('logout.get');
+
+//ユーザーーコントローラー
+Route::group(['middleware' => ['auth']], function () {
+   Route::resource('users', 'UsersController', ['only' => ['index', 'show']]); 
+});
